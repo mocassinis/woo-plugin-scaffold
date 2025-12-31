c896da143e8a655209c4316f2999069e27e3d6af
Nevma.gr <gkastorinis@gmail.com>
2025-12-31 05:59:57 +0200

    Replace Analog logger with WooCommerce logger
    - Update Logger.php to use WooCommerce's built-in logger (wc_get_logger)
    - Add helper methods: debug(), info(), warning(), error()
    - Remove Analog dependency from Gruntfile.js
    - Logs will appear in WooCommerce > Status > Logs with source 'plugin-tpl'

--- php/Logger.php
+++ php/Logger.php
@@ -6,11 +6,6 @@
 namespace Xkon\Plugin_Tpl;
 
 /**
- * Import necessary classes.
- */
-use \Analog\Analog;
-
-/**
  * Check that the file is not accessed directly.
  */
 if ( ! defined( 'ABSPATH' ) ) {
@@ -18,18 +13,36 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Class Log.
+ * Class Logger.
  *
- * Simple wrapper for Analog.
+ * Wrapper for WooCommerce Logger.
  */
 class Logger {
 	/**
-	 * Log file.
+	 * Logger instance.
 	 *
-	 * @return string
+	 * @var \WC_Logger_Interface
 	 */
-	public static function log_file() {
-		return wp_normalize_path( WP_CONTENT_DIR . '/' . 'plugin-tpl.log' );
+	private static $logger;
+
+	/**
+	 * Log source identifier.
+	 *
+	 * @var string
+	 */
+	private static $source = 'plugin-tpl';
+
+	/**
+	 * Get logger instance.
+	 *
+	 * @return \WC_Logger_Interface
+	 */
+	private static function get_logger() {
+		if ( null === self::$logger ) {
+			self::$logger = wc_get_logger();
+		}
+
+		return self::$logger;
 	}
 
 	/**
@@ -42,22 +55,60 @@ class Logger {
 	}
 
 	/**
-	 * Log.
+	 * Log a message.
 	 *
-	 * @param mixed $message
+	 * @param mixed  $message Message to log.
+	 * @param string $level   Log level (debug, info, notice, warning, error, critical, alert, emergency).
 	 */
-	public static function log( $message ) {
+	public static function log( $message, $level = 'info' ) {
 		if ( ! self::should_log() ) {
 			return;
 		}
 
-		if ( ! is_string( $message ) || is_array( $message ) || is_object( $message ) ) {
+		if ( ! function_exists( 'wc_get_logger' ) ) {
+			return;
+		}
+
+		if ( ! is_string( $message ) ) {
 			$message = print_r( $message, true );
 		}
 
-		// Set Analog.
-		Analog::handler( self::log_file() );
+		self::get_logger()->log( $level, $message, array( 'source' => self::$source ) );
+	}
 
-		Analog::log( $message );
+	/**
+	 * Log debug message.
+	 *
+	 * @param mixed $message Message to log.
+	 */
+	public static function debug( $message ) {
+		self::log( $message, 'debug' );
+	}
+
+	/**
+	 * Log info message.
+	 *
+	 * @param mixed $message Message to log.
+	 */
+	public static function info( $message ) {
+		self::log( $message, 'info' );
+	}
+
+	/**
+	 * Log warning message.
+	 *
+	 * @param mixed $message Message to log.
+	 */
+	public static function warning( $message ) {
+		self::log( $message, 'warning' );
+	}
+
+	/**
+	 * Log error message.
+	 *
+	 * @param mixed $message Message to log.
+	 */
+	public static function error( $message ) {
+		self::log( $message, 'error' );
 	}
 }
